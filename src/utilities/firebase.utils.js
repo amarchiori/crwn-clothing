87954e56059644298029c2d48b3c8678b665e9b3
Alessandra <alessm96@gmail.com>
2022-12-27 12:25:31 -0500

    added categories route from Shop

--- src/utilities/firebase.utils.js
+++ src/utilities/firebase.utils.js
@@ -15,6 +15,10 @@ import {
     doc, 
     getDoc,
     setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs,
 } 
 from 'firebase/firestore';
 
@@ -40,6 +44,30 @@ const firebaseConfig = {
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
   export const db =  getFirestore();
+
+    export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+        const collectionRef = collection(db, collectionKey);
+        const batch = writeBatch(db);
+
+        objectsToAdd.forEach((object) => {
+            const docRef =doc(collectionRef, object.title.toLowerCase());
+            batch.set(docRef, object);
+        });
+        await batch.commit();
+        console.log('done');
+    }
+
+    export const getCategoriesandDocuments = async () => {
+        const collectionRef = collection(db, 'categories');
+        const q = query(collectionRef);
+        const querySnapshot = await getDocs(q);
+        const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+            const { title, items } = docSnapshot.data();
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {})
+        return categoryMap;
+    }
 
   export const createUserDocumentFromAuth = async (
     userAuth,
